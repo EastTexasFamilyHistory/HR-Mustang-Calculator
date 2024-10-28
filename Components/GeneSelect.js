cf89b1100a2e820067f1cad5f2977a39077fd05b
EastTexasFamilyHistory <ponykw@gmail.com>
2024-10-28 16:39:53 -0500

    Update GeneSelect.js

--- Components/GeneSelect.js
+++ Components/GeneSelect.js
@@ -1,1 +1,23 @@
+import React from 'react';
+import '../styles/inputs.css';
 
+const GeneSelect = ({ label, options, value, onChange }) => {
+  return (
+    <div className="gene-select">
+      <label className="gene-label">
+        {label}
+      </label>
+      <select 
+        value={value} 
+        onChange={(e) => onChange(e.target.value)}
+        className="gene-dropdown"
+      >
+        {options.map(option => (
+          <option key={option} value={option}>{option}</option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default GeneSelect;
